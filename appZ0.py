b149291769a46f76850d231f39f361f846554b11
atujaycee <atujaycee@yahoo.com>
2025-06-22 21:46:14 +0100

    Update appZ0.py

--- appZ0.py
+++ appZ0.py
@@ -42,7 +42,7 @@ col1, col2 = st.columns([1, 6])
 with col1:
     st.image("Nmite_Logo.jpg", width=100)
 with col2:
-    st.title("📊 NMITE Excel Feedback Cleaner")
+    st.title("📊 NMITE Excel Feedback Cleaner Merger")
 st.markdown("Upload multiple Excel feedback files, clean and merge them effortlessly.")
 
 # Core Cleaning Function
